6c9a18b97245205755bded777b421a94155a22e4
mingquan.li <mingquan.li@5262.com>
2018-03-29 14:48:14 +0800

    改变超能力的同时，我还得重新制造个超人。效率太低了！

---
+++ public/nb/iocdi/2多元化超能力/Force.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * 超能力--蛮力，属性有：力量值
+ * User: kevin
+ * Date: 2018/3/29
+ * Time: 11:36
+ */
+class Force
+{
+    //力量值
+    protected $force;
+
+
+    public function __construct($force)
+    {
+        $this->force = $force;
+    }
+
+}
