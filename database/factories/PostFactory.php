0da75017121502c86f4f5ac3834a548a45cb616c
mingquan.li <mingquan.li@5262.com>
2018-07-02 22:29:06 +0800

    publish_at default bug fix fix

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -9,6 +9,6 @@ $factory->define(App\Post::class, function (Faker $faker) {
         'title' => $faker->sentence(mt_rand(3,10)),
         'content' => join("",$faker->paragraphs(mt_rand(3,6))),
 //        'publish_at' => strtotime($faker->dateTimeBetween('-1 month','+3 days'))
-        'publish_at' => mt_rand(strtotime('-1 month'),strtotime('+3 days'))
+        'published_at' => mt_rand(strtotime('-1 month'),strtotime('+3 days'))
     ];
 });
