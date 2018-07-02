8e6593e8a1c41d18468b7333bfd591fd36a914f3
mingquan.li <mingquan.li@5262.com>
2018-07-02 22:52:46 +0800

    protected function getDateFormat()     {         return 'U';     }

--- app/Post.php
+++ app/Post.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+
+    protected function getDateFormat()
+    {
+        return 'U';
+    }
+
     protected $dates = ['published_at'];
 
     public function setTitleAttribute($value)
