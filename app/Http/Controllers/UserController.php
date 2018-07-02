2b37122ac26afdf73a15c4b1d4215515f53256ff
limq <limq@cgnfs.com.cn>
2018-07-02 11:48:26 +0800

    定义控制器

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,4 +7,8 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     //
+    public function show($id)
+    {
+        return view('user.profile',['user'=> User::findOrFail($id)]);
+    }
 }
