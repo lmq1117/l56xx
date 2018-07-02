ca7cb5369e159c88ecd6a347f553712495782582
mingquan.li <mingquan.li@5262.com>
2018-07-02 22:26:18 +0800

    publish_at default

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,10 +3,12 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
+    var_dump()
     return [
         //
         'title' => $faker->sentence(mt_rand(3,10)),
         'content' => join("",$faker->paragraphs(mt_rand(3,6))),
-        'publish_at' => strtotime($faker->dateTimeBetween('-1 month','+3 days'))
+//        'publish_at' => strtotime($faker->dateTimeBetween('-1 month','+3 days'))
+        'publish_at' => mt_rand(strtotime('-1 month'),strtotime('+3 days'))
     ];
 });
