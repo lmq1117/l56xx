00b0c7468b07e3b5124639fed3e8317ed2ca957b
limq <limq@cgnfs.com.cn>
2018-07-06 17:00:18 +0800

    hr

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,8 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in!<hr>
+                    2018
                 </div>
             </div>
         </div>
