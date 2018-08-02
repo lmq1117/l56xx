42a4294dc35711a9c540162cddf5030f33a5bd5c
limq <limq@cgnfs.com.cn>
2018-08-02 15:57:06 +0800

    rds

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class TestController extends Controller
 {
-    //
+
+    public function rds()
+    {
+        $user = Redis::get('name');
+        return $user;
+    }
 }
